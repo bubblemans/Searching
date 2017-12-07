ec832c1b51268cb239ef3258d7e86abc903c854a
bubblemans <f89647779@gmail.com>
2017-12-07 11:09:52 -0800

    first commit

---
+++ Final expected question 1/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    string sentence = "I am Hung Yeh Lin. How are you?";
+    string keyword;
+    bool found = false;
+    
+    cout << "Enter a keyword:\n";
+    cin >> keyword;
+    for (int i = 0; i < sentence.length(); i++) {
+        if (sentence[i] == keyword[0]) {
+            for (int k = 0; k < keyword.length(); k++) {
+                if (sentence[i+k] == keyword[k]){
+                    found = true;
+                }
+                else
+                    found = false;
+            }
+            if (found){
+                cout << "I found it.\n";
+                break;
+            }
+            else
+                cout <<"I didn't find it.\n";
+        }
+    }
+}
