07b8a93738b2b2c339f73442d6a093cbe9e1f3d5
bubblemans <f89647779@gmail.com>
2017-12-08 15:17:23 -0800

    add tellrelevant

---
+++ tellrelevant/tellrelevant/main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+
+int search(string, string);
+int main() {
+    string sentence1 = "I am Hung Yeh Lin. How are you?";
+    string sentence2 = "I am Hung Yeh Lin. I work in Hung company.";
+    int sentence1num, sentence2num;
+    string keyword;
+    
+    cout << "Enter a keyword:\n";
+    cin >> keyword;
+    sentence1num = search(sentence1, keyword);
+    sentence2num = search(sentence2, keyword);
+    if ((sentence1num == 0)&&(sentence2num == 0)) {
+        cout << "I didn't find it.\n";
+    }
+    else if (sentence1num > sentence2num) {
+        cout << "The sentence 1 is more relevant.\n";
+        cout << sentence1 << endl;
+    }
+    else if (sentence1num < sentence2num){
+        cout << "The sentence 2 is more relevant.\n";
+        cout << sentence2 << endl;
+    }
+    else
+        cout << "They are relevant equaaly.\n";
+}
+
+int search(string sen, string key){
+    int count = 0;
+    bool found = false;
+    
+    for (int i = 0; i < sen.length(); i++) {
+        if (sen[i] == key[0]) {
+            for (int k = 0; k < key.length(); k++) {
+                if (sen[i+k] == key[k]){
+                    found = true;
+                }
+                else
+                    found = false;
+            }
+            if (found)
+                count++;
+        }
+    }
+    return count;
+}
